f7fc375053ca9b37786b8ddb1d294c65a2c32323
Yuri Fushiki <y.fushiki@apptime.co.jp>
2020-01-09 13:49:55 +0900

    [AppTime] iosのinfo.plstをUnity上で編集(ios13以上のBluetooth設定ダイアログ文言、上のステータスバーテキストの文字色を白に)

---
+++ Assets/Editor/XcodeSettingsPostProcesser.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+using System.Collections;
+
+
+public class XcodeSettingsPostProcesser {
+
+	[PostProcessBuild]
+	public static void SetXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+	{
+		if (buildTarget != BuildTarget.iOS) return;
+
+		var plistPath = pathToBuiltProject + "/Info.plist";
+		var plist = new PlistDocument();
+		plist.ReadFromString(File.ReadAllText(plistPath));
+
+		var rootDict = plist.root;
+		// ここに記載したKey-ValueがXcodeのinfo.plistに反映されます
+		rootDict.SetString("UIStatusBarStyle", "UIStatusBarStyleLightContent");
+
+		rootDict.SetString("NSBluetoothAlwaysUsageDescription", "別の端末にデータを送信する為にBluetoothを使用します");
+		File.WriteAllText(plistPath, plist.WriteToString());
+	}
+}
